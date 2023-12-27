3f62d353895ae521ffb5215d8592d6b2b68ca993
beaibanez02 <bea.ibanez02@gmail.com>
2023-12-27 17:17:01 +0100

    arreglos imagenes sirenas
    se notaba el cambio con js.Resize en ps

--- js/script.js
+++ js/script.js
@@ -15,12 +15,52 @@ function menu() {
     }
 }
 
-        // HAMBURGUER
-        $(".hamburger").click(function() {
-            $(this).toggleClass('close');
-          });
+// HAMBURGUER
+$(".hamburger").click(function() {
+    $(this).toggleClass('close');
+});
 
 
+          
+$(document).ready(function(){
+
+    $("#elene1").click(function(){
+        $(".cambio1").attr("src","img/ELENE/abalorios.png")
+    });
+
+
+    $("#lyra1").click(function(){
+        $(".cambio2").attr("src","img/LYRa/lyra1.png")
+    });
+
+
+    // ALEXIA ALEXIA ALEXIA
+
+    $("#alexia1").click(function(){
+        $(".cambio3").attr("src","img/ALEXIA/alexia11.png")
+    });
+
+    $("#alexia2").click(function(){
+        $(".cambio3").attr("src","")
+    });
+
+    $("#alexia3").click(function(){
+        $(".cambio3").attr("src","")
+    });
+
+    $("#alexia4").click(function(){
+        $(".cambio3").attr("src","")
+    });
+
+    $("#alexia5").click(function(){
+        $(".cambio3").attr("src","")
+    });
+
+    $("#alexia6").click(function(){
+        $(".cambio3").attr("src","")
+    });
+
+});
     
 
 
