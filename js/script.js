6cc5651dc61c70c149e4fee36cceeb36118126fe
SOFIA <ssofiamonteroo@gmail.com>
2024-01-09 15:54:38 +0100

    MAIN FINAL

--- js/script.js
+++ js/script.js
@@ -269,3 +269,46 @@ function obtenerNumeroFavart(id) {
 
 
 
+
+
+
+/*POP UPS apartado de CUENTA*/
+
+/* PEDIDOS */
+
+function mostrarVentana() {
+    var ventana = document.getElementById('ventana');
+    ventana.style.display = 'block';
+}
+
+function cerrarVentana() {
+    var ventana = document.getElementById('ventana');
+    ventana.style.display = 'none';
+}
+
+/* PUNTOS ACUMULADOS */
+
+
+function mostrarMispuntos() {
+    var ventana = document.getElementById('puntuacion');
+    ventana.style.display = 'block';
+}
+
+function cerrarMispuntos() {
+    var ventana = document.getElementById('puntuacion');
+    ventana.style.display = 'none';
+}
+
+/* MIS DATOS */
+
+
+function mostrarMisdatos() {
+    var ventana = document.getElementById('informate');
+    ventana.style.display = 'block';
+}
+
+function cerrarMisdatos() {
+    var ventana = document.getElementById('informate');
+    ventana.style.display = 'none';
+}
+
